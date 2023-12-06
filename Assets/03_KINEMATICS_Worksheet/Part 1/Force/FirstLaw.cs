b4cda3a860444c97f4c8cd867b7c8795b9de8ef7
ivanfright <71205975+ivanfright@users.noreply.github.com>
2023-12-07 00:25:52 +0800

    Kin Q1 n Q2

--- Assets/03_KINEMATICS_Worksheet/Part 1/Force/FirstLaw.cs
+++ Assets/03_KINEMATICS_Worksheet/Part 1/Force/FirstLaw.cs
@@ -1,21 +1,23 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class FirstLaw : MonoBehaviour
-// {
-//     public Vector3 force;
-//     Rigidbody rb;
+public class FirstLaw : MonoBehaviour
+{
+    public Vector3 force;
+    Rigidbody rb;
 
-//     void Start()
-//     {
-//         rb = // your code here;
-//         rb.// your code here;
-//     }
+    void Start()
+    {
+        // Get the Rigidbody component
+        rb = GetComponent<Rigidbody>();
+        // Add force to the Rigidbody
+        rb.AddForce(force);
+    }
 
-//     void FixedUpdate()
-//     {
-//         Debug.Log(transform.position);
-//     }
-// }
+    void FixedUpdate()
+    {
+        Debug.Log(transform.position);
+    }
+}
 
